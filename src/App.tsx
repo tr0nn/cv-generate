6a7b7e036a2d94a3020b728dacf3e2f76bf4eb42
tr0nn <bakarnemsicveridze@gmail.com>
2023-02-21 11:46:50 +0400

    chore: main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,44 @@
 import React from 'react';
-
+import { useState } from 'react';
+//import styled from 'styled-components';
 //import logo2 from './images/LOGO1.npg';
-//import './App.css';
+import './styles/App.css';
 
 function App() {
-  return (
-    <div>
-      <header className="App-header">
-        <img src={require('./images/shutt.png')} alt="f" />
-      </header>
-    </div>
-  );
+  const [homePageState, setHomePageState] = useState(true);
+
+  const Home = () => {
+    return (
+      <div className="main-div">
+        <img
+          className="main-image"
+          src={require('./images/shutt.png')}
+          alt="mainImage"
+        />
+
+        <img
+          className="signature"
+          src={require('./images/LOGO1.png')}
+          alt="signature"
+        />
+        <img
+          className="redberry-text"
+          src={require('./images/LOGO2.png')}
+          alt="redberry"
+        />
+        <button
+          onClick={() => {
+            setHomePageState(false);
+          }}
+          className="add-rasume-btn"
+        >
+          რეზიუმეს დამატება
+        </button>
+        <hr className="hr" />
+      </div>
+    );
+  };
+  return <div>{homePageState ? <Home /> : null}</div>;
 }
 
 export default App;
